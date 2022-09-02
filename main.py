48e323ad68a30432802489d7f6e777bd7fd83619
unknown <maier.samuel99@gmail.com>
2022-09-02 21:05:26 +0300

    Added an if else statement to show if the guessed number is too high or low and a message when the guess is correct.

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
-        guess = input(f"Guess a number between 1 and {x}.")
-        print(guess)
+        guess = int(input(f"Guess a number between 1 and {x}."))
+        if guess < random_number:
+            print("Sorry, guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high.")
+
+    print(f"Congrats, you have guessed the number {random_number} correnctly.")
+
 
 guess(10)
